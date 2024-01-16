57a68c3b6f7e8fd15a743387385b986b2ffca0d6
lnirjar <lnirjar@gmail.com>
2024-01-16 20:40:16 +0530

    completed

---
+++ backend/models/userModel.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+}, {
+  timestamps: true
+});
+
+// here we are using function keyword instead of the arrow function because of the way the `this` keyword works
+userSchema.pre('save', async function (next) {
+  // here this refers to the calling object like user doc
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
